1b0f97e619b965f1bb66b6d0241d3b23bcc98a9b
Bayu Beta Brahmantio <bayubrahmantio@gmail.com>
2019-10-13 15:47:23 +0200

    added parallel argument

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -1,30 +1,39 @@
+library(parallel)
 
-brute_force_knapsack = function(x, W){
+brute_force_knapsack = function(x, W, parallel = FALSE){
   
   stopifnot(is.data.frame(x), length(colnames(x)) == 2, colnames(x) == c("w", "v"), W>=0)
   
-  # create all possible subsets
-  subsets = list()
-  p = 1
-  for (i in 1:nrow(x)){
-    sets = combn(x = 1:nrow(x), m = i, simplify = FALSE)
-    for (j in 1:length(sets)){
-      subsets[[p]] = sets[[j]]
-      p = p+1
-    }
+  if (parallel == FALSE){
+    
+    # calculate the sum of weights and values from all possible subsets
+    total_weight = unlist(lapply(1:nrow(x), FUN = function(i){combn(x[,1], i, sum)}))
+    total_value = unlist(lapply(1:nrow(x), FUN = function(i){combn(x[,2], i, sum)}))
+    subsets = unlist(lapply(1:nrow(x), FUN = function(i){combn(1:nrow(x), m = i, paste, collapse = "")}))
+    
+    # find the maximum value given the knapsack size
+    max_index = which(total_value == max(total_value[which(total_weight<=W)]))
+    
+    return(list(value = round(total_value[max_index]), elements = as.numeric(strsplit(subsets[max_index], "")[[1]])))
   }
   
-  # calculate the total weight and value for each subset
-  total_value = c()
-  total_weight = c()
-  for (i in 1:length(subsets)){
-    total_weight[i] = sum(x[,1][subsets[[i]]])
-    total_value[i] = sum(x[,2][subsets[[i]]])
+  
+  else{
+    # make clusters
+    cores = detectCores()
+    cl = makeCluster(cores, type = "PSOCK")
+    
+    # calculate the sum of weights and values from all possible subsets
+    total_weight = unlist(parLapply(cl, 1:nrow(x), fun = function(i){combn(x[,1], i, sum)}))
+    total_value = unlist(parLapply(cl, 1:nrow(x), fun = function(i){combn(x[,2], i, sum)}))
+    subsets = unlist(parLapply(cl, 1:nrow(x), fun = function(i){combn(1:nrow(x), m = i, paste, collapse = "")}))
+    
+    stopCluster(cl)
+    
+    # find the maximum value given the knapsack size
+    max_index = which(total_value == max(total_value[which(total_weight<=W)]))
+    
+    return(list(value = round(total_value[max_index]), elements = subsets[[max_index]]))
   }
-  
-  # find the maximum value given the knapsack size
-  max_index = which(total_value == max(total_value[which(total_weight<=W)]))
-  
-  return(list(value = round(total_value[max_index]), elements = subsets[[max_index]]))
   
 }
